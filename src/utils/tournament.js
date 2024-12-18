526f744cc16270099cb1a9c7e9d5ff754ee3c093
geraldine.rouy <geraldine.rouy@gmail.com>
2024-12-18 08:47:00 +0100

    Refactoring tournament.js (contains functions to generation elimination rounds):
    -function shuffleCurrentPlayersList done

--- src/utils/tournament.js
+++ src/utils/tournament.js
@@ -1,35 +1,13 @@
-export function generateFirstRound(players) {
-    console.log("Players sent in generateFirstRound :", players);
+//functions to generate new rounds
 
-    const shuffledPlayers = shuffle(players);
-
-    let matchesForNextRound = createMatches(shuffledPlayers);
-
-    return matchesForNextRound;
+//takes current players list and mix
+function shuffleCurrentPlayersList(currentPlayersArray) {
+    currentPlayersArray.sort(() => Math.random() - 0.5);
 }
 
-export function generateNextRound(players) {
-    const shuffledPlayers = shuffle(players);
-    const matchesForNextRound = createMatches(shuffledPlayers);
+//takes mixed players list and makes pairs
+function makePlayersPairs(playersArray) {
 
-    // if (players.length !== matchesForNextRound.length) {
-    //     alert("Please select a winner for each match !");
-    //     return;
-    // }
-
-    console.log("Matches for next Round : ", matchesForNextRound);
-
-    this.winners = [];
 }
 
-function shuffle(array) {
-    return array.sort(() => Math.random() - 0.5);
-}
-
-function createMatches(players) {
-    const matches = [];
-    for (let i = 0; i < players.length; i += 2) {
-        matches.push([players[i], players[i + 1] || 'No player found : wait next round to play and support your friends \\o/']);
-    }
-    return matches;
-}
+//returns pairs (matchs) of all current players
